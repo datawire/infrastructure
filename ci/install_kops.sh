99a3d6a15de64f2302ede88d3302a1e9b8a8274f
Philip Lombardi <plombardi@datawire.io>
2017-08-02 13:42:40 -0400

    Do not hard code kops version

--- ci/install_kops.sh
+++ ci/install_kops.sh
@@ -5,7 +5,7 @@ KOPS_VERSION=${KOPS_VERSION:?"ERROR: KOPS_VERSION not set"}
 KOPS_SHA256SUM=${KOPS_SHA256SUM:?"ERROR: KOPS_SHA256SUM not set"}
 
 curl -L --output /tmp/kops \
-     https://github.com/kubernetes/kops/releases/download/1.6.2/kops-linux-amd64
+     https://github.com/kubernetes/kops/releases/download/${KOPS_VERSION}/kops-linux-amd64
 
 echo "${KOPS_SHA256SUM}  /tmp/kops" > /tmp/kops_SHA256
 sha256sum -c /tmp/kops_SHA256
