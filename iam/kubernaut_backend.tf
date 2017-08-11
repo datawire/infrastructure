ce860d354007f0c1b11267cc42c51f65b3f1093b
Philip Lombardi <plombardi@datawire.io>
2017-08-11 13:36:37 -0400

    IAM

---
+++ iam/kubernaut_backend.tf
@@ -0,0 +1,19 @@
+// file: iam/kubernaut_backend.tf
+
+resource "aws_iam_policy" "kubernaut_backend_policy" {
+  name        = "kubernaut"
+  path        = "/cloud/"
+  description = "policy for kubernaut_backend"
+  policy      = "${file("${path.module}/kubernaut_backend-policy.json")}"
+}
+
+resource "aws_iam_user" "kubernaut_backend" {
+  name = "kubernaut_backend"
+  path = "/cloud/"
+}
+
+resource "aws_iam_policy_attachment" "kubernaut-attach" {
+  name       = "kubernaut_backend_policy_attachment"
+  users      = ["${aws_iam_policy.kubernaut_backend_policy.name}"]
+  policy_arn = "${aws_iam_policy.kubernaut_backend_policy.arn}"
+}
